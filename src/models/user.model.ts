043a2f4156b64d11eb908a4d7a715a348a4ff399
C-bv <charles.bouveret.dev@gmail.com>
2023-02-01 13:51:56 +0100

    ✨ [mongoose] add user model with methods and validators

---
+++ src/models/user.model.ts
@@ -0,0 +1,71 @@
+import { IRole } from '@custom-types/custom-types';
+import bcrypt from 'bcrypt';
+import mongoose, { Document, Model, Schema } from 'mongoose';
+
+interface IUser {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    role: IRole;
+};
+
+interface IUserDocument extends IUser, Document {
+    setPassword: (password: string) => Promise<void>;
+    checkPassword: (password: string) => Promise<boolean>;
+};
+
+interface IUserModel extends Model<IUserDocument> {
+    isEmailTaken(email: string): Promise<boolean>;
+};
+
+const UserSchema: Schema<IUserDocument> = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: (email: string) => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email),
+            message: 'Email is not valid'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (password: string) => password.length >= 6,
+            message: 'Password must be at least 6 characters long'
+        }
+    },
+    role: { type: String, enum: [] as IRole[], default: 'user' }
+});
+
+/*
+    use regular function instead of arrow function because arrow function does not bind 'this'
+*/
+UserSchema.methods.setPassword = async function (password: string) {
+    const salt: string = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    this.password = hash;
+};
+
+UserSchema.methods.checkPassword = async function (password: string) {
+    const result = await bcrypt.compare(password, this.hashedPassword);
+    return result;
+};
+
+UserSchema.statics.isEmailTaken = async function (email: string) {
+    const user = await this.findOne({ email });
+    return !!user;
+};
+
+UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+    await this.setPassword(this.password);
+    console.log('🚩', this);
+});
+
+const User = mongoose.model<IUserDocument, IUserModel>('User', UserSchema);
+export default User;
